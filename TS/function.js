c4b1da1ce9191033dac7406896f0b8cd46cdecf8
Vinícius Blandy <viny19381@hotmail.com>
2023-08-28 08:36:48 -0300

    primeiro commit

---
+++ TS/function.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplica(10, 20));
+function pegaNome(nome) {
+    console.log(`Ola ${nome}`);
+}
+pegaNome('Vinicius');
---
+++ TS/function.ts
@@ -0,0 +1,11 @@
+function multiplica(n1:number, n2:number): number {
+    return n1 * n2
+}
+
+console.log(multiplica(10,20))
+
+function pegaNome(nome:string):void {
+    console.log(`Ola ${nome}`)
+}
+
+pegaNome('Vinicius')
